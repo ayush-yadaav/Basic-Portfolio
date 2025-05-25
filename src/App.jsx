7d0123e068f0dc52c1375b8b35199cae9628c238
ayush-yadaav <csayush27@gmail.com>
2025-05-25 21:55:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/project" element={<Project />} />
           <Route path="/contact" element={<Contact />} />
