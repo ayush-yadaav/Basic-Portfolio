f35b9f77d9dfe8697d3b0b2d1a8448c6f0d41e24
ayush-yadaav <csayush27@gmail.com>
2025-05-25 21:52:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <div className='bg-zinc-900 min-h-screen w-full text-white'>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/project" element={<Project />} />
           <Route path="/contact" element={<Contact />} />
